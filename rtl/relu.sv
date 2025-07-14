18c5d40537d9068f5db113dd46230073f4e6f35d
ChiefAbdu <abdullahwaraich2004@gmail.com>
2025-07-14 13:53:15 +0500

    Fixes

--- rtl/relu.sv
+++ rtl/relu.sv
@@ -1,15 +1,15 @@
 // ReLU Module: output = max(0, input)
 
 module relu (
-    input  logic signed [19:0] in,   // Signed input
-    output logic signed [19:0] out   // ReLU output
+    input  logic signed [15:0] in,   // Signed input
+    output logic signed [15:0] out   // ReLU output
 );
 
     always_comb begin
         if (in[19])
             out = 0;
         else
-            out = in >>> 12;
+            out = in >>> 8;
     end
 
 endmodule
